a5e336d1de4a7e708ae4176ebc18c3c4ac859d8f
Lazaneq <lazanowski.artur@gmail.com>
2023-05-01 22:20:43 +0200

    #1 Start

---
+++ src/test/java/com/ToDoApp/ToDoAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ToDoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
